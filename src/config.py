3442a17399ec37519a749677e79c72b872c5aede
lequan2902 <benjamin.lequan@gmail.com>
2022-11-17 02:32:08 +0800

    Add Labels Dict & Seq Length

--- src/config.py
+++ src/config.py
@@ -9,12 +9,13 @@ MODEL_PATH = MAIN_PATH / "artifacts"
 
 ### Sound Processing ###
 SAMPLE_RATE = 16000
+PADDING_LENGTH = 16000
 FFT_WINDOW = 512
 FFT_OVERLAP = 128
 EPS = 1e-10
 NO_CHANNELS = FFT_WINDOW // 2 + 1  ### Max Frequency = SAMPLE_RATE / 2 https://en.wikipedia.org/wiki/Nyquist_rate
 MEL_CHANNELS = 128
-SEQUENCE_LEN = (SAMPLE_RATE - FFT_OVERLAP) // FFT_OVERLAP + 2
+SEQUENCE_LEN = (PADDING_LENGTH - FFT_OVERLAP) // FFT_OVERLAP + 2
 
 ### Training Parameters ###
 NO_EPOCHS = 10
@@ -22,3 +23,17 @@ BATCH_SIZE = 32
 LEARNING_RATE = 3e-4
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 NUM_WORKERS = os.cpu_count()
+MEAN = 0.5
+STD = 0.5
+
+### 
+LABELS = ['silence','yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go',
+          'bed', 'bird', 'cat', 'dog', 'eight', 'five', 'four', 'happy', 'house', 'marvin',
+          'nine', 'one', 'seven', 'sheila', 'six', 'three', 'tree', 'two', 'wow', 'zero']
+
+LABEL2ID = {}
+ID2LABEL = {}
+
+for idx, label in enumerate(LABELS):
+    LABEL2ID[label] = idx
+    ID2LABEL[idx] = label
