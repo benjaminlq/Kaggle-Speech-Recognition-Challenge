1b3fa52167d257f93f86806d70aa085a3c0b5c74
lequan2902 <benjamin.lequan@gmail.com>
2022-11-19 16:47:57 +0800

    Move train files to engine

--- src/dev/train.py
+++ src/dev/train.py
@@ -1,7 +1,7 @@
 import argparse
 import config
 
-from dev.engine import train_classification, train_translation
+from dev.engine import train_classification, train_translation, eval_classification
 from dev.dataloader import SpeechDataLoader
 
 def get_argument_parser():
@@ -25,7 +25,7 @@ def get_argument_parser():
         help="How many epochs you need to run (default: 10)",
         type=int,
         default=config.NO_EPOCHS,
-    )
+        )
 
     parser.add_argument(
         "-bs",
@@ -33,7 +33,7 @@ def get_argument_parser():
         help="The number of images in a batch (default: 64)",
         type=int,
         default=config.BATCH_SIZE,
-    )
+        )
 
     parser.add_argument(
         "-lr",
@@ -41,7 +41,7 @@ def get_argument_parser():
         help="The learning rate used for optimizer (default: 1e-4)",
         type=float,
         default=config.LEARNING_RATE,
-    )
+        )
     
     parser.add_argument(
         "-md",
@@ -49,7 +49,7 @@ def get_argument_parser():
         help="Type of model instance used for training",
         type=str,
         default="inceptiontime",
-    )
+        )
     
     parser.add_argument(
         "-l",
@@ -57,12 +57,26 @@ def get_argument_parser():
         help="Load Model from previous ckpt",
         type=str,
         default=False
-    )
+        )
+    
+    parser.add_argument(
+        "-es",
+        "--earlystop",
+        help="Whether EarlyStop during model training",
+        type=bool,
+        default=False
+        )
+    
+    parser.add_argument(
+        "-p",
+        "--patience",
+        help="How Many epoches for Early Stopping",
+        type=int,
+        default=5
+        )
 
     args = parser.parse_args()
     return args
-
-
     
 if __name__ == "__main__":
     args = get_argument_parser()
@@ -71,6 +85,8 @@ if __name__ == "__main__":
     learning_rate = args.learning_rate
     model_type = args.model
     load_model = args.load
+    early_stopping = args.earlystop
+    patience = args.patience
     
     data_manager = SpeechDataLoader(batch_size = batch_size)
     data_manager.setup()
@@ -81,9 +97,13 @@ if __name__ == "__main__":
     model = config.MODEL_PARAMS[model_type]["instance"](**config.MODEL_PARAMS[model_type]["params"])
     
     if config.MODEL_PARAMS[model_type]["type"] == "classification":
-        train_classification(model, train_loader, val_loader, epochs_no, learning_rate, load = load_model)
+        train_classification(model, train_loader, val_loader, epochs_no, learning_rate, early_stopping = early_stopping, patience = patience, load = load_model)
     elif config.MODEL_PARAMS[model_type]["type"] == "translation":
-        train_translation(model, train_loader, val_loader, epochs_no, learning_rate, load = load_model)
-        
+        train_translation(model, train_loader, val_loader, epochs_no, learning_rate, early_stopping = early_stopping, patience = patience,  load = load_model)
+    
+    # Evaluate result on test dataset
+    test_loader = data_manager.test_loader()
+    test_loss, test_acc = eval_classification(model, test_loader)
+    print(f"Test Loss = {test_loss}, Test Accuracy = {test_acc}")
     
 ## python3 src/dev/train.py -e 1 -bs 128
