e37b0fd7f1ba6636cdb842aed3b37a45a4be2e9d
benjaminlq <benjamin.lequan@gmail.com>
2022-11-20 13:57:12 +0800

    Add logging and docstring

--- src/dev/engine.py
+++ src/dev/engine.py
@@ -1,11 +1,16 @@
-import config
+"""Functions for training and evaluating
+"""
+from typing import Callable, Tuple
+
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from typing import Callable
 from tqdm import tqdm
-import warnings
+
+import config
+from config import LOGGER
 from dev import utils
+
 
 def train_classification(
     model: Callable,
@@ -17,28 +22,42 @@ def train_classification(
     patience: int = 5,
     save: bool = True,
     load: bool = False,
-    ):
-    
-    optimizer = torch.optim.Adam(params = model.parameters(), lr = learning_rate)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.5, patience = 3)
+):
+    """Train model as a Multiclass Classification model using CrossEntropyLoss.
+
+    Args:
+        model (Callable): Model to train
+        train_loader (DataLoader): Training Dataloader
+        val_loader (DataLoader): Validatation Dataloader
+        epochs_no (int): No of epochs
+        learning_rate (float): Learning Rate
+        early_stopping (bool, optional): Whether to use EarlyStopping. Defaults to False.
+        patience (int, optional): How many epochs without val_acc improvement for EarlyStopping. Defaults to 5.
+        save (bool, optional): Save model. Defaults to True.
+        load (bool, optional): Load model from checkpoint before training. Defaults to False.
+    """
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, factor=0.5, patience=3
+    )
     criterion = nn.CrossEntropyLoss()
-    
+
     model.to(config.DEVICE)
-    print(f"Training models on {config.DEVICE}")
-    
+    LOGGER.info(f"Training models on {config.DEVICE}")
+
     if load:
         ckpt_path = config.MODEL_PATH / "model_ckpt" / str(model) / "model.pt"
         if not ckpt_path.exists():
-            warnings.warn("Ckpt_path does not exist. Training New Model")
+            LOGGER.info("Ckpt_path does not exist. Training New Model")
         utils.load_model(model, ckpt_path)
-    
+
     best_val_acc = 0
     patience_count = 0
-    
+
     for epoch in range(epochs_no):
         model.train()
         epoch_loss = 0
-        tk0 = tqdm(train_loader, total = len(train_loader))
+        tk0 = tqdm(train_loader, total=len(train_loader))
         for batch_idx, (inputs, targets) in enumerate(tk0):
             optimizer.zero_grad()
             outs = model(inputs.to(config.DEVICE, dtype=torch.float))
@@ -46,16 +65,22 @@ def train_classification(
             epoch_loss += loss.item()
             loss.backward()
             optimizer.step()
-            
+
             if (batch_idx + 1) % 100 == 0:
-                print(f"Epoch {epoch + 1}, Batch {batch_idx + 1} Loss = {loss.item()}")
-            
+                LOGGER.info(
+                    f"Epoch {epoch + 1}, Batch {batch_idx + 1} Loss = {loss.item()}"
+                )
+
         val_loss, val_acc = eval_classification(model, val_loader)
-        print(f"Epoch {epoch + 1}: Train Loss = {epoch_loss/len(train_loader)}, Val Loss = {val_loss}, Val Accuracy = {val_acc}")
+        LOGGER.info(
+            f"Epoch {epoch + 1}: Train Loss = {epoch_loss/len(train_loader)}, Val Loss = {val_loss}, Val Accuracy = {val_acc}"
+        )
         scheduler.step(val_loss)
-        
+
         if val_acc > best_val_acc:
-            print(f"Validation Accuracy Improved from {best_val_acc} to {val_acc}")
+            LOGGER.info(
+                f"Validation Accuracy Improved from {best_val_acc} to {val_acc}"
+            )
             best_val_acc = val_acc
             patience_count = 0
             if save:
@@ -64,30 +89,43 @@ def train_classification(
                     model_path.mkdir()
                 ckpt_path = str(model_path / "model.pt")
                 utils.save_model(model, ckpt_path)
-                
+
         else:
-            print(f"Validation Accuracy from epoch {epoch + 1} did not improve")
+            LOGGER.info(f"Validation Accuracy from epoch {epoch + 1} did not improve")
             patience_count += 1
             if early_stopping and patience_count == patience:
-                print(f"No val acc improvement for {patience} consecutive epochs. Early Stopped at epoch {epoch + 1}")
+                LOGGER.info(
+                    f"No val acc improvement for {patience} consecutive epochs. Early Stopped at epoch {epoch + 1}"
+                )
                 break
-                        
-def eval_classification(model, val_loader):
+
+
+def eval_classification(model: Callable, val_loader: Callable) -> Tuple:
+    """Evaluate Model
+
+    Args:
+        model (Callable): Model
+        val_loader (Callable): Validation Dataloader
+
+    Returns:
+        Tuple: (val_loss, val_accuracy)
+    """
     model.eval()
     val_loss = 0
     total_count = 0
     correct_count = 0
     with torch.no_grad():
-        tk0 = tqdm(val_loader, total = len(val_loader))
+        tk0 = tqdm(val_loader, total=len(val_loader))
         for inputs, targets in tk0:
             outs = model(inputs.to(config.DEVICE, dtype=torch.float))
             loss = nn.CrossEntropyLoss()(outs, targets.to(config.DEVICE))
             val_loss += loss.item()
-            preds = torch.argmax(outs, dim = 1).detach().cpu()
+            preds = torch.argmax(outs, dim=1).detach().cpu()
             total_count += len(targets)
             correct_count += (preds == targets).sum().item()
-    
+
     return val_loss / len(val_loader), correct_count / total_count * 100
+
 
 def train_translation(
     model: Callable,
@@ -97,15 +135,31 @@ def train_translation(
     learning_rate: float,
     save: bool = True,
     load: bool = False,
-    ):
-    
-    optimizer = torch.optim.Adam(params=model.parameters(), lr = learning_rate)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor = 0.5, patience = 15)
-    
-    model.to(config.DEVICE)
-    if load:
-        ckpt_path = config.MODEL_PATH / str(model) / "model.pt"
-        if not ckpt_path.exist():
-            warnings.warn("Ckpt_path does not exist. Training New Model")
-        model.load_state_dict(torch.load(str(ckpt_path), map_location=config.DEVICE))
-        
+):
+    """Train model as a Translation Seq-To_Seq model using CTCLoss.
+
+    Args:
+        model (Callable): Model to be trained
+        train_loader (DataLoader): Training DataLoader
+        val_loader (DataLoader): Validation Dataloader
+        epochs_no (int): No of epochs
+        learning_rate (float): Learning Rate
+        save (bool, optional): Save model checkpoint. Defaults to True.
+        load (bool, optional): Load model from checkpoint before training. Defaults to False.
+    """
+
+    # optimizer = torch.optim.Adam(params=model.parameters(), lr=learning_rate)
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+    #     optimizer, factor=0.5, patience=15
+    # )
+
+    # model.to(config.DEVICE)
+    # if load:
+    #     ckpt_path = config.MODEL_PATH / "model_ckpt" / str(model) / "model.pt"
+    #     if not ckpt_path.exist():
+    #         LOGGER.info("Ckpt_path does not exist. Training New Model")
+    #     model.load_state_dict(torch.load(str(ckpt_path), map_location=config.DEVICE))
+
+    # for epoch in range(epochs_no):
+    #     model.train()
+    return
--- src/dev/train.py
+++ src/dev/train.py
@@ -1,8 +1,12 @@
+"""Traing Script
+"""
 import argparse
-import config
 
-from dev.engine import train_classification, train_translation, eval_classification
+import config
+from config import LOGGER
 from dev.dataloader import SpeechDataLoader
+from dev.engine import eval_classification, train_classification, train_translation
+
 
 def get_argument_parser():
     """
@@ -25,7 +29,7 @@ def get_argument_parser():
         help="How many epochs you need to run (default: 10)",
         type=int,
         default=config.NO_EPOCHS,
-        )
+    )
 
     parser.add_argument(
         "-bs",
@@ -33,7 +37,7 @@ def get_argument_parser():
         help="The number of images in a batch (default: 64)",
         type=int,
         default=config.BATCH_SIZE,
-        )
+    )
 
     parser.add_argument(
         "-lr",
@@ -41,43 +45,40 @@ def get_argument_parser():
         help="The learning rate used for optimizer (default: 1e-4)",
         type=float,
         default=config.LEARNING_RATE,
-        )
-    
+    )
+
     parser.add_argument(
         "-md",
         "--model",
         help="Type of model instance used for training",
         type=str,
         default="inceptiontime",
-        )
-    
+    )
+
     parser.add_argument(
-        "-l",
-        "--load",
-        help="Load Model from previous ckpt",
-        type=str,
-        default=False
-        )
-    
+        "-l", "--load", help="Load Model from previous ckpt", type=str, default=False
+    )
+
     parser.add_argument(
         "-es",
         "--earlystop",
         help="Whether EarlyStop during model training",
         type=bool,
-        default=False
-        )
-    
+        default=False,
+    )
+
     parser.add_argument(
         "-p",
         "--patience",
         help="How Many epoches for Early Stopping",
         type=int,
-        default=5
-        )
+        default=5,
+    )
 
     args = parser.parse_args()
     return args
-    
+
+
 if __name__ == "__main__":
     args = get_argument_parser()
     batch_size = args.batch_size
@@ -87,23 +88,44 @@ if __name__ == "__main__":
     load_model = args.load
     early_stopping = args.earlystop
     patience = args.patience
-    
-    data_manager = SpeechDataLoader(batch_size = batch_size)
+
+    model = config.MODEL_PARAMS[model_type]["instance"](
+        **config.MODEL_PARAMS[model_type]["params"]
+    )
+
+    data_manager = SpeechDataLoader(batch_size=batch_size)
     data_manager.setup()
-    train_loader, val_loader = data_manager.train_loader(), data_manager.validation_loader()
-    # model = InceptionTime(in_channels = config.MEL_CHANNELS,
-    #                       sequence_len = config.SEQUENCE_LEN,
-    #                       num_classes = len(config.LABELS))
-    model = config.MODEL_PARAMS[model_type]["instance"](**config.MODEL_PARAMS[model_type]["params"])
-    
+    train_loader, val_loader = (
+        data_manager.train_loader(),
+        data_manager.validation_loader(),
+    )
+
     if config.MODEL_PARAMS[model_type]["type"] == "classification":
-        train_classification(model, train_loader, val_loader, epochs_no, learning_rate, early_stopping = early_stopping, patience = patience, load = load_model)
+        train_classification(
+            model,
+            train_loader,
+            val_loader,
+            epochs_no,
+            learning_rate,
+            early_stopping=early_stopping,
+            patience=patience,
+            load=load_model,
+        )
     elif config.MODEL_PARAMS[model_type]["type"] == "translation":
-        train_translation(model, train_loader, val_loader, epochs_no, learning_rate, early_stopping = early_stopping, patience = patience,  load = load_model)
-    
+        train_translation(
+            model,
+            train_loader,
+            val_loader,
+            epochs_no,
+            learning_rate,
+            early_stopping=early_stopping,
+            patience=patience,
+            load=load_model,
+        )
+
     # Evaluate result on test dataset
     test_loader = data_manager.test_loader()
     test_loss, test_acc = eval_classification(model, test_loader)
-    print(f"Test Loss = {test_loss}, Test Accuracy = {test_acc}")
-    
+    LOGGER.info(f"Test Loss = {test_loss}, Test Accuracy = {test_acc}")
+
 ## python3 src/dev/train.py -e 1 -bs 128
