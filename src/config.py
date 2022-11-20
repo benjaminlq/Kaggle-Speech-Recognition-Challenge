4ae13a2edce6a9f0af8aada49b074f5169c8f4fe
benjaminlq <benjamin.lequan@gmail.com>
2022-11-20 13:55:25 +0800

    Add file loggers

--- src/config.py
+++ src/config.py
@@ -1,8 +1,19 @@
-from pathlib import Path
-import torch
+"""Configurations files containing settings:
+1. Path Directories
+2. Settings for processing sound files
+3. Training Parameters
+4. Dictionaries to translate labels/chars to encoded integers
+5. Settings for Logger
+"""
+import logging
 import os
-from dev.models.inceptiontime import InceptionTime
+import sys
+from pathlib import Path
+
+import torch
+
 from dev.models.ctcmodel import CTCModel
+from dev.models.inceptiontime import InceptionTime
 
 ### Path ###
 MAIN_PATH = Path(__file__).resolve().parents[1]
@@ -15,7 +26,9 @@ PADDING_LENGTH = 16000
 FFT_WINDOW = 512
 FFT_OVERLAP = 128
 EPS = 1e-10
-NO_CHANNELS = FFT_WINDOW // 2 + 1  ### Max Frequency = SAMPLE_RATE / 2 https://en.wikipedia.org/wiki/Nyquist_rate
+NO_CHANNELS = (
+    FFT_WINDOW // 2 + 1
+)  # Max Frequency = SAMPLE_RATE / 2 https://en.wikipedia.org/wiki/Nyquist_rate
 MEL_CHANNELS = 128
 SEQUENCE_LEN = (PADDING_LENGTH - FFT_OVERLAP) // FFT_OVERLAP + 2
 
@@ -29,9 +42,39 @@ MEAN = 0.5
 STD = 0.5
 
 ### LABELS & IDS
-LABELS = ['silence','yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go',
-          'bed', 'bird', 'cat', 'dog', 'eight', 'five', 'four', 'happy', 'house', 'marvin',
-          'nine', 'one', 'seven', 'sheila', 'six', 'three', 'tree', 'two', 'wow', 'zero']
+LABELS = [
+    "silence",
+    "yes",
+    "no",
+    "up",
+    "down",
+    "left",
+    "right",
+    "on",
+    "off",
+    "stop",
+    "go",
+    "bed",
+    "bird",
+    "cat",
+    "dog",
+    "eight",
+    "five",
+    "four",
+    "happy",
+    "house",
+    "marvin",
+    "nine",
+    "one",
+    "seven",
+    "sheila",
+    "six",
+    "three",
+    "tree",
+    "two",
+    "wow",
+    "zero",
+]
 
 LABEL2ID = {}
 ID2LABEL = {}
@@ -39,33 +82,53 @@ ID2LABEL = {}
 for idx, label in enumerate(LABELS):
     LABEL2ID[label] = idx
     ID2LABEL[idx] = label
-    
+
 all_chars = set()
 for label in LABELS[1:]:
     for char in label:
         all_chars.add(char)
 
 CHAR_LIST = list(all_chars)
-CHAR2ID = {"$":0}
-ID2CHAR = {0:"$"}
+CHAR2ID = {"$": 0}
+ID2CHAR = {0: "$"}
 
 charid = 1
 for char in CHAR_LIST:
     CHAR2ID[char] = charid
     ID2CHAR[charid] = char
-    
+
 ### Model Definition
-MODEL_PARAMS = {"inceptiontime":{"instance": InceptionTime,
-                                 "transform": "mfcc",
-                                 "params": {"in_channels" : MEL_CHANNELS,
-                                            "sequence_len" : SEQUENCE_LEN,
-                                            "num_classes" : len(LABELS)},
-                                 "type": "classification",
-                                 },
-                
-                "ctcmodel":{"instance": CTCModel,
-                            "transform": "mfcc",
-                            "params": {"vocab_size": len(CHAR_LIST),
-                                       "channels_no": MEL_CHANNELS},
-                            "type": "translation",},
-                }
+MODEL_PARAMS = {
+    "inceptiontime": {
+        "instance": InceptionTime,
+        "transform": "mfcc",
+        "params": {
+            "in_channels": MEL_CHANNELS,
+            "sequence_len": SEQUENCE_LEN,
+            "num_classes": len(LABELS),
+        },
+        "type": "classification",
+    },
+    "ctcmodel": {
+        "instance": CTCModel,
+        "transform": "mfcc",
+        "params": {"vocab_size": len(CHAR_LIST), "channels_no": MEL_CHANNELS},
+        "type": "translation",
+    },
+}
+
+### Logging configurations
+LOGGER = logging.getLogger(__name__)
+
+stream_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler(
+    filename=str(MODEL_PATH / "model_ckpt" / "logfile.log")
+)
+
+formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
+file_handler.setFormatter(formatter)
+# stream_handler.setFormatter(formatter)
+
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(stream_handler)
+LOGGER.addHandler(file_handler)
