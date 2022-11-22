8c2e0deab0d5cf59687fb67a1d08dd9044ddf07f
lequan2902 <benjamin.lequan@gmail.com>
2022-11-23 02:31:17 +0800

    rnn.py

---
+++ src/dev/models/rnn.py
@@ -0,0 +1,10 @@
+"""Basic RNN model
+"""
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+
+# class RNNClassification(nn.Module):
+#     def __init__(self, in_channels: int,
+#                  ):
